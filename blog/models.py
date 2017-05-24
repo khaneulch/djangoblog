6626ea1476f7d99de3bbf972bbaab80317bc414b
Your Name <you@example.com>
2017-05-24 17:22:56 +0900

    170524

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,9 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    upload_img = models.FileField()
 
     def publish(self):
         self.published_date = timezone.now()
