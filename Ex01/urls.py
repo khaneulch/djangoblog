b5b934f98f286574929d66b9d2318d9bc16a2aa1
Your Name <you@example.com>
2017-05-29 18:16:20 +0900

    17.05.29_02

--- Ex01/urls.py
+++ Ex01/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
-]
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
